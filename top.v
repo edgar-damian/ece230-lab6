1a126329e38ff7d9473a14b783fcbf0d05b71c2c
Audrey Cruz <audreycruz@u.boisestate.edu>
2026-02-24 11:32:43 -0700

    update top.v

--- top.v
+++ top.v
@@ -1,5 +1,3 @@
-// Implement top level module
-// Implement top level module
 module top(
     input [7:0] sw,
     output [5:0] led
@@ -7,8 +5,8 @@ module top(
 );
     
     light light_inst(
-        .downStairs(sw[0]),
-        .upStairs(sw[1]),
+        .downstairs(sw[0]),
+        .upstairs(sw[1]),
         .stair_light(led[0])    
     );
     
@@ -16,23 +14,23 @@ module top(
         .A(sw[2]),
         .B(sw[3]),
         .Y(led[1]),
-        .carry(led[2])
+        .Carry(led[2])
     );
     
-    wire c1;
+    wire w1;
     
     full_adder LSBfull_adder_inst(
         .A(sw[4]),
         .B(sw[6]),
         .Cin(1'b0),
         .Y(led[3]),
-        .Cout(c1)
+        .Cout(w1) 
     );
     
     full_adder MSBfull_adder_inst(
         .A(sw[5]),
         .B(sw[7]),
-        .Cin(c1),
+        .Cin(w1),
         .Y(led[4]),
         .Cout(led[5])
     
